eeb1520a07dbb51146d5a9f05d5b3f5d864d7e10
Name <email@example.ru>
2021-01-16 15:14:26 +0300

    Добавьте файлы проекта.

---
+++ final_practice_questionnaire/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace final_practice_questionnaire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Как вас зовут:");
+            var name = Console.ReadLine();
+            Console.Write("Сколько вам лет:");
+            var age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Вас зовут {0}, вам {1} лет", name, age);
+            Console.Write("Когда вы родились:");
+            var birthday = Console.ReadLine();
+            Console.WriteLine("Вы родились {0}", birthday);
+
+        }
+    }
+}
